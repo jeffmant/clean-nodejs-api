d7d95cfd5c07e7440a4efa9877f6818207f306fa
jeffmant <jgsmantovani@gmail.com>
2022-06-18 14:54:10 -0300

    refactor: remove code duplication

--- src/presentation/helpers/validators/required-field.validation.ts
+++ src/presentation/helpers/validators/required-field.validation.ts
@@ -2,11 +2,7 @@ import { MissingParamError } from '../../errors'
 import { Validation } from './validation'
 
 export class RequiredFieldValidation implements Validation {
-  private readonly fieldName: string
-
-  constructor (fieldName: string) {
-    this.fieldName = fieldName
-  }
+  constructor (private readonly fieldName: string) {}
 
   validate (input: any): Error | undefined {
     if (!input[this.fieldName]) {
