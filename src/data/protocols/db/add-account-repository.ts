bf060eee757e7291eb808773d7d1e5fa940f2548
jeffmant <jgsmantovani@gmail.com>
2022-06-18 15:41:52 -0300

    refactor: separate protocols interface folders

--- src/data/protocols/db/add-account-repository.ts
+++ src/data/protocols/db/add-account-repository.ts
@@ -1,4 +1,4 @@
-import { AccountModel, AddAccountModel } from '../useCases/add-account/db-add-account-protocols'
+import { AccountModel, AddAccountModel } from '../../useCases/add-account/db-add-account-protocols'
 
 export interface AddAccountRepository {
   add: (accountData: AddAccountModel) => Promise<AccountModel>
