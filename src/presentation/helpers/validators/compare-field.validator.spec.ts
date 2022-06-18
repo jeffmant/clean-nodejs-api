98733bceb7380dbb6b98cf93817b5b380a70771b
jeffmant <jgsmantovani@gmail.com>
2022-06-18 13:24:41 -0300

    refactor: add tests to compare fields validation

---
+++ src/presentation/helpers/validators/compare-field.validator.spec.ts
@@ -0,0 +1,19 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-field.validation'
+
+const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation =>
+  new CompareFieldsValidation(field, fieldToCompare)
+
+describe('Compare Fields Validator', () => {
+  it('Should return Missing Param error if validation fails', () => {
+    const sut = makeSut('field', 'fieldToCompare')
+    const error = sut.validate({ name: 'any_value', fieldToCompare: 'another_value' })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  it('Should not return error if validation passes', () => {
+    const sut = makeSut('field', 'field')
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
