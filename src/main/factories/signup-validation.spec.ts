128503b27d8bb9a427719565722088966dcb4451
jeffmant <jgsmantovani@gmail.com>
2022-06-18 10:54:01 -0300

    refactor: move required field validation to the validation composite

---
+++ src/main/factories/signup-validation.spec.ts
@@ -0,0 +1,20 @@
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field.validaiton'
+import { Validation } from '../../presentation/helpers/validators/validation'
+import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
+import { makeSignupValidation } from './signup-validation'
+
+jest.mock('../../presentation/helpers/validators/validation-composite')
+
+describe('Signup Validation Factory', () => {
+  it('Should call ValidationComposite with all validations', () => {
+    makeSignupValidation()
+
+    const validations: Validation[] = []
+
+    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -6,14 +6,25 @@ import { SignupController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { LogControllerDecorator } from '../decorators/log'
+import { makeSignupValidation } from './signup-validation'
 
 export const makeSignupController = (): Controller => {
   const salt = 12
-  const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
+
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+
   const accountMongoRepository = new AccountMongoRepository()
+
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signupController = new SignupController(emailValidatorAdapter, dbAddAccount)
+
+  const signupController = new SignupController(
+    emailValidatorAdapter,
+    dbAddAccount,
+    makeSignupValidation()
+  )
+
   const logMongoErrorRepository = new LogMongoErrorRepository()
+
   return new LogControllerDecorator(signupController, logMongoErrorRepository)
 }
