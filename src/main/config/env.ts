f6d4e06a6fab7baabe6752377699fdfd5d5d0db0
jeffmant <jgsmantovani@gmail.com>
2022-06-19 07:47:39 -0300

    refactor: add dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,9 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default {
-  PORT: process.env.PORT ?? '5050',
-  MONGO_URI: process.env.MONGO_URI ?? 'mongodb://docker:mongopw@localhost:55004',
-  JWT_SECRET: process.env.JWT_SECRET ?? 'DJE*92==king-dom'
+  PORT: process.env.PORT ?? '7000',
+  MONGO_URI: process.env.MONGO_URI ?? 'mongodb://localhost:27017/clean-node-api',
+  JWT_SECRET: process.env.JWT_SECRET ?? 'secret'
 }
