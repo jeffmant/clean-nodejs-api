1ade370bbe045413ecc2fc84e1ddeb0c9aaf0d41
jeffmant <jgsmantovani@gmail.com>
2022-06-24 20:43:39 -0300

    test: ensure AuthMiddleware calls GetAccountByToken with correct values

--- src/presentation/middlewares/auth/auth.middlware.spec.ts
+++ src/presentation/middlewares/auth/auth.middlware.spec.ts
@@ -1,11 +1,57 @@
 import { forbidden } from '../../helpers/http/http-helper'
 import { AccessDeniedError } from '../../errors'
 import { AuthMiddleware } from './auth.middleware'
+import { HttpRequest } from '../../protocols'
+import { GetAccountByToken } from '../../../domain/useCases/getAccountByToken'
+import { AccountModel } from '../../../domain/models/account.model'
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@email.com',
+  password: 'valid_password'
+})
+
+const makeGetAccountByTokenStub = (): GetAccountByToken => {
+  class GetAccountByTokenStub implements GetAccountByToken {
+    async get (accessToken: string, role?: string): Promise<AccountModel> {
+      return await new Promise((resolve) => resolve(makeFakeAccount()))
+    }
+  }
+
+  return new GetAccountByTokenStub()
+}
+
+interface SutTypes {
+  sut: AuthMiddleware
+  getAccountByTokenStub: GetAccountByToken
+}
+
+const makeSut = (): SutTypes => {
+  const getAccountByTokenStub = makeGetAccountByTokenStub()
+  const sut = new AuthMiddleware(getAccountByTokenStub)
+  return {
+    sut,
+    getAccountByTokenStub
+  }
+}
 
 describe('Auth Middleware', () => {
   it('Should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  it('Should call GetAccountByToken with correct accessToken', async () => {
+    const { sut, getAccountByTokenStub } = makeSut()
+    const getSpy = jest.spyOn(getAccountByTokenStub, 'get')
+    const httpRequest: HttpRequest = {
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(getSpy).toHaveBeenCalledWith('any_token')
   })
 })
