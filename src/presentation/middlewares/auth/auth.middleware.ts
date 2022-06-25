0c89347b64799cd066b0d0654de10f19a5930255
jeffmant <jgsmantovani@gmail.com>
2022-06-24 21:06:21 -0300

    feat: ensure AuthMiddleware returns 500 if GetAccountByToken throws

--- src/presentation/middlewares/auth/auth.middleware.ts
+++ src/presentation/middlewares/auth/auth.middleware.ts
@@ -5,14 +5,15 @@ import { HttpRequest, HttpResponse, Middlware } from '../../protocols'
 
 export class AuthMiddleware implements Middlware {
   constructor (
-    private readonly getAccountByToken: GetAccountByToken
+    private readonly getAccountByToken: GetAccountByToken,
+    private readonly role?: string
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const accessToken = httpRequest.headers?.['x-access-token']
       if (accessToken) {
-        const account = await this.getAccountByToken.get(accessToken)
+        const account = await this.getAccountByToken.get(accessToken, this.role)
         if (account) {
           return ok({ accountId: account.id })
         }
