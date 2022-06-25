b0e032ab5404b013042d703411940bff774610b5
jeffmant <jgsmantovani@gmail.com>
2022-06-24 21:48:42 -0300

    feat: ensure DbAccountByToken calls GetAccountByTokenREpository with correct values

--- src/data/useCases/get-account-by-token/get-account-by-tone.usecase.ts
+++ src/data/useCases/get-account-by-token/get-account-by-tone.usecase.ts
@@ -1,14 +1,19 @@
 import { GetAccountByToken } from '../../../domain/useCases/getAccountByToken'
 import { Decrypter } from '../../protocols/criptography/decrypter'
 import { AccountModel } from '../add-account/db-add-account.protocols'
+import { GetAccountByTokenRepository } from '../../protocols/db/account/get-account-by-token.repository'
 
 export class DbGetAccountByToken implements GetAccountByToken {
   constructor (
-    private readonly decrypter: Decrypter
+    private readonly decrypter: Decrypter,
+    private readonly getAccountByTokenRepository: GetAccountByTokenRepository
   ) {}
 
   async get (accessToken: string, role?: string): Promise<AccountModel> {
-    await this.decrypter.decrypt(accessToken)
+    const token = await this.decrypter.decrypt(accessToken)
+    if (token) {
+      await this.getAccountByTokenRepository.getByToken(accessToken, role)
+    }
     return await new Promise(resolve => resolve(null))
   }
 }
