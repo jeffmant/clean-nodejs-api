f608f2164d0635bc87c08b3fd8a84841636ec765
jeffmant <jgsmantovani@gmail.com>
2022-07-22 21:35:06 -0300

    refactor: change api name

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -5,6 +5,6 @@ dotenv.config()
 export default {
   PORT: Number(process.env.PORT) ?? 7000,
   API_VERSION: String(process.env.API_VERSION) ?? 'v1',
-  MONGO_URL: String(process.env.MONGO_URL) ?? 'mongodb://localhost:27017/clean-node-api',
+  MONGO_URL: String(process.env.MONGO_URL) ?? 'mongodb://localhost:27017/clean-node-ts-api',
   JWT_SECRET: String(process.env.JWT_SECRET) ?? 'secret'
 }
